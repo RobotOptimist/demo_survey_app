2f3c95dde3468eb9228406904a8849da9c54556f
James MacIvor <james.macivor1@gmail.com>
2020-09-07 23:00:00 -0400

    created form and stuff

--- pages/index.vue
+++ pages/index.vue
@@ -1,83 +1,42 @@
 <template>
-  <div class="container">
-    <div>
-      <h1 class="title">
-        Fire Hydrant Surveyor
-      </h1>
-      <div class="links">
-        <b-button
-          v-if="!$auth.loggedIn"
-          variant="primary"
-          size="lg"
-          @click="login"
-        >
-          Login
-        </b-button>
-        <b-button
-          v-else
-          variant="warning"
-          @click="logout"
-          size="lg"
-        >
-          Logout
-        </b-button>
-      </div>
-    </div>
-  </div>
+  <b-container class="pt-5">
+    <b-row align-h="center" class="mt-5">
+      <b-col cols="*">
+        <h1 class="title">Fire Hydrant Surveyor</h1>
+      </b-col>
+    </b-row>
+    <b-row align-h="center" v-if="$auth.loggedIn">
+      <b-col sm="9" class="my-4">
+        <survey-form @submitForm="handleFormResult"></survey-form>
+      </b-col>
+    </b-row>
+    <b-row align-h="center" class="mt-3">
+      <b-col cols="*">
+        <b-button v-if="!$auth.loggedIn" variant="primary" size="lg" @click="login">Login</b-button>
+        <b-button v-else variant="warning" @click="logout" size="lg">Logout</b-button>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import surveyForm from '../components/survey-form'
+import { mapGetters } from "vuex";
 export default {
+  components: [
+    surveyForm
+  ],
   methods: {
     login() {
-      this.$auth.loginWith('auth0')
+      this.$auth.loginWith("auth0");
     },
     logout() {
       this.$auth.logout();
+    },
+    async handleFormResult(formObj) {
+      
     }
   },
-  computed: mapGetters(['isAuthenticated']),
-}
+  computed: mapGetters(["isAuthenticated"]),
+};
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
